66499ae5b22c599f004597f4a52380a3999e3469
Martin Jordanov <xxm0703@abv.bg>
2017-03-26 11:58:55 +0300

    Hide Objects

---
+++ scripts/CameraDrag.js
@@ -0,0 +1,9 @@
+﻿var speed : float;
+
+function Update(){
+	if(Input.GetMouseButton(0)){
+		transform.position.x -= Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+		transform.position.z -= Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+	}
+
+}
---
+++ scripts/Toggle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle : MonoBehaviour {
+    public void hide(GameObject obj)
+    {
+        if (obj.active == false)
+            obj.SetActive(true);
+        else
+            obj.SetActive(false);
+    }
+}
